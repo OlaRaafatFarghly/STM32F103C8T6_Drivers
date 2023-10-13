0b11a93c390ee6b78de0b25ba8b7b154e844318e
OlaRaafatFarghly <olarafat2024@gmail.com>
2023-10-13 14:34:10 +0300

    Updating GPIO driver

---
+++ Version 1/COTS/MCAL/GPIO/Inc/MGPIO_private.h
@@ -0,0 +1,21 @@
+#ifndef GPIO_PRIVATE_H
+#define GPIO_PRIVATE_H
+
+#define CNF_PIN_SHIFTING          2u
+
+
+#define GPIO_PERIPPHERAL_NUM      8u
+
+#define CR_MASK                   0b11
+#define CR_PIN_ACCESS             2u
+#define CR_PIN_SHIFTING           8u
+
+#define ODR_MASK                  0b1
+
+#define BSRR_MASK                  0b1
+#define BSRR_PIN_SHIFTING            16u
+
+
+#define IDR_MASK                  0b1
+
+#endif
